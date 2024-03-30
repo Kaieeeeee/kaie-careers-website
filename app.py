041c8bf635f7cbc7faa20d87fcc248e071628043
Kaieeeeee <100998026+Kaieeeeee@users.noreply.github.com>
2024-03-30 09:02:17 +0000

    added job in .py then added jobs on html

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Quezon City, Philippines',
+    'salary':' 10,000 Pesos'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Caloocan City, Philippines',
+    'salary':' 15,000 Pesos'
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remote',
+    'salary':' 12,000 Pesos'
+  },
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'location':'New York, USA',
+    'salary':'$15,000'
+  },
+  
+]
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",jobs=JOBS)
   
 print(__name__)
 if __name__ == "__main__":
